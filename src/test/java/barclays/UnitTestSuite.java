b1278d7c060f6f689f54b7de888de6dfd4a6ecad
pravinlipne <pravin.lipne@gmail.com>
2021-08-23 18:17:36 +0530

    first commit

---
+++ src/test/java/barclays/UnitTestSuite.java
@@ -0,0 +1,15 @@
+package barclays;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+   SampleUnitTest.class,
+   PrivateUnitTest.class
+})
+
+public class UnitTestSuite {
+
+}
